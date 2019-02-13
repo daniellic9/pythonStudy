3cdc383da983ce68605bf23f22d2f84666cef7fc
Danielli Costa <daniellic9@gmail.com>
2019-02-12 22:00:15 -0200

    update array

--- array.py
+++ array.py
@@ -9,6 +9,7 @@ input_str = input()
 dimensions=[int(x) for x in input_str.split(',')]
 rowNum=dimensions[0]
 colNum=dimensions[1]
+#[[inicializa com zeros por colunas] inicializa vetores pelas 'linhas']
 multilist = [[0 for col in range(colNum)] for row in range(rowNum)]
 
 for row in range(rowNum):
