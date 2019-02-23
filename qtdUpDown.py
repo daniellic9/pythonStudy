e860d684bbc8cd4ad8769950df995acaaa876ba2
Danielli Costa <daniellic9@gmail.com>
2019-02-22 21:30:18 -0300

    update

--- qtdUpDown.py
+++ qtdUpDown.py
@@ -1,3 +1,10 @@
+#Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters.
+#Suppose the following input is supplied to the program:
+#Hello world!
+#Then, the output should be:
+#UPPER CASE 1
+#LOWER CASE 9
+
 s = input()
 d={"UPPER CASE":0, "LOWER CASE":0}
 for c in s:
@@ -9,3 +16,4 @@ for c in s:
         pass
 print("UPPER CASE", d["UPPER CASE"])
 print("LOWER CASE", d["LOWER CASE"])
+
